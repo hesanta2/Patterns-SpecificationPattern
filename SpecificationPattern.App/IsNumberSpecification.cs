05e80c92733482e7faf9bd330df0db1d1bb33b3d
hesanta2 <hesanta@gmail.com>
2017-01-19 13:28:52 +0100

    Add Specification pattern implementation. And. Or. Not.

---
+++ SpecificationPattern.App/IsNumberSpecification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern.App
+{
+    public class IsNumberSpecification : CompositeSpecification<int>
+    {
+        private int number;
+
+        public IsNumberSpecification(int number)
+        {
+            this.number = number;
+        }
+
+        public override Expression<Func<int, bool>> SpecExpression
+        {
+            get
+            {
+                return (obj => obj == number);
+            }
+        }
+    }
+}
--- SpecificationPattern.App/Program.cs
+++ SpecificationPattern.App/Program.cs
@@ -10,6 +10,13 @@ namespace SpecificationPattern.App
     {
         static void Main(string[] args)
         {
+
+            ISpecification<int> spec1 = new IsNumberSpecification(2);
+            ISpecification<int> spec2 = new IsPairNumberSecification();
+
+            ISpecification<int> resultSpec = spec1.And(spec2).Not().Or(spec1);
+
+            Console.WriteLine($"{resultSpec.SpecExpression} = {resultSpec.IsSatisfiedBy(2)}");
         }
     }
 }
